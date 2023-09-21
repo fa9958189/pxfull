adb83bfea5a9feb3ec975ad210411833ed1a5b5e
felipe <130618161+fa9958189@users.noreply.github.com>
2023-09-20 21:54:03 -0300

    c
    c

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-// Função para mostrar o popup
-function mostrarPopup() {
-    var popup = document.getElementById('popup');
-    popup.style.display = 'block';
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const modalButtons = document.querySelectorAll('.modal-button');
+    modalButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const targetModalId = button.getAttribute('data-modal-target');
+            const targetModal = document.getElementById(targetModalId);
+            if (targetModal) {
+                targetModal.style.display = 'block';
+            }
+        });
+    });
 
-// Função para fechar o popup
-function fecharPopup() {
-    var popup = document.getElementById('popup');
-    popup.style.display = 'none';
-}
-
-// Adicionar evento de clique ao botão "Mostrar Popup"
-var showPopupButton = document.getElementById('showPopup');
-showPopupButton.addEventListener('click', mostrarPopup);
-
-// Adicionar evento de clique ao botão de fechar o popup
-var closePopupButton = document.getElementById('closePopup');
-closePopupButton.addEventListener('click', fecharPopup);
+    const modalCloseButtons = document.querySelectorAll('.modal-close-button');
+    modalCloseButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const targetModalId = button.getAttribute('data-modal-close');
+            const targetModal = document.getElementById(targetModalId);
+            if (targetModal) {
+                targetModal.style.display = 'none';
+            }
+        });
+    });
+});
