3c306b67e8d41eac4569be55d94d300f714b8e98
felipe <130618161+fa9958189@users.noreply.github.com>
2024-08-22 14:27:57 -0300

    atualizaçao
    atualizaçao

--- script.js
+++ script.js
@@ -47,3 +47,37 @@ window.addEventListener('click', (event) => {
 });
 
 
+function toggleSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('open-menu');
+}
+    function enviarMensagemWhatsApp() {
+        // Número de telefone no formato internacional (no exemplo, +55 63 9203-6652)
+        var numeroTelefone = '556392036652';
+
+        // Mensagem opcional
+        var mensagem = 'Olá, estou entrando em contato pelo site.';
+
+        // Montar o link do WhatsApp
+        var linkWhatsApp = 'https://api.whatsapp.com/send?phone=' + numeroTelefone;
+
+        // Adicionar a mensagem se desejar
+        if (mensagem) {
+            linkWhatsApp += '&text=' + encodeURIComponent(mensagem);
+        }
+
+        // Redirecionar o usuário para o link do WhatsApp
+        window.location.href = linkWhatsApp;
+}
+    function abrirPerfilInstagram() {
+        // Nome de usuário do Instagram (no exemplo, "ambientalanjos")
+        var usuarioInstagram = 'ambientalanjos';
+
+        // Montar o link do Instagram
+        var linkInstagram = 'https://www.instagram.com/' + usuarioInstagram;
+
+        // Redirecionar o usuário para o link do Instagram
+        window.location.href = linkInstagram;
+}
+
+
